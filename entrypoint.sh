b07acb9eea215745904260a48d349434e9f3a4c0
hoainamnv34 <hoainam.nv34@gmail.com>
2024-06-04 01:16:02 +0700

    update git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 
+git config --global --add safe.directory /github/workspace
+
 gitleaks detect --source="$1" --report-path="$2" -v --report-format="$3" --config="/gitleaks-custom.toml"
